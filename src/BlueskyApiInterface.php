2d2640c6bf875bb2e6641cce0fc2087f1d934430
Stefan Keßeler <poti@kaoz.org>
2023-10-20 21:03:23 +0200

    fix: add missing createRecord method to api interface

--- src/BlueskyApiInterface.php
+++ src/BlueskyApiInterface.php
@@ -4,7 +4,12 @@ declare(strict_types=1);
 
 namespace potibm\Bluesky;
 
+use potibm\Bluesky\Feed\Post;
+use potibm\Bluesky\Response\CreateRecordResponse;
+
 interface BlueskyApiInterface
 {
     public function getDidForHandle(string $handle): string;
+
+    public function createRecord(Post $post): CreateRecordResponse;
 }
